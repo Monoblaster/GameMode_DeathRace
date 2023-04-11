13e645bee3d0a570bd595f9099089bf4af4d4bb1
Monoblaster <64797269+Monoblaster@users.noreply.github.com>
2023-04-10 19:48:42 -0700

    oops removed player

--- support/Support_NewStereo.cs
+++ support/Support_NewStereo.cs
@@ -294,6 +294,7 @@ package VehicleStereo
 
 	function serverCmdLove(%c)
 	{
+		%p = %c.player;
 		if($VehicleStereoLoveBypass || !isObject(%p) || !isObject(%mount = %p.GetBaseMount()) || !%mount.getDataBlock().rideable)
 		{
 			$VehicleStereoLoveBypass = false;
